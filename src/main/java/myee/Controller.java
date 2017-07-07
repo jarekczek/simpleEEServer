3b307b5a378511a940f8bb18d1c0040642697404
Jarek Czekalski <jaroslaw.czekalski@bonsoft.pl>
2017-07-07 07:57:46 +0200

    fixed servlet mapping, need /*, not /

--- src/main/java/myee/Controller.java
+++ src/main/java/myee/Controller.java
@@ -1,6 +1,7 @@
 package myee;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -9,17 +10,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/")
+@WebServlet(urlPatterns = "/*")
 public class Controller extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-    throws ServletException, IOException {
-    if (req.getPathInfo().startsWith("/h")) {
+  throws ServletException, IOException
+  {
+    Logger log = Logger.getLogger("bs");
+    String path = Optional.ofNullable(req.getPathInfo())
+      .orElse("/");
+    if (path.startsWith("/h")) {
       html(req, resp);
       return;
     }
-    Logger log = Logger.getLogger("bs");
     log.info("alive");
     resp.getWriter().println("hello");
   }
